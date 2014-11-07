3128fc250a3e2d23df73aad03da1ebb2e94b1e8f
PanzerFR <p.malma@hotmail.fr>
2014-11-07 17:47:48 +0100

    Corrections sur VueAccueil (Layout et encodage)

--- thesaurus/src/thesaurus/VueAccueil.java
+++ thesaurus/src/thesaurus/VueAccueil.java
@@ -2,23 +2,28 @@ package thesaurus;
 
 import java.awt.Color;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 
-public class VueAccueil extends View {
+public class VueAccueil extends View 
+{
 	
 	private JTextPane textAccueil;
 
-	public VueAccueil() {
+	public VueAccueil() 
+	{
 		textAccueil = new JTextPane();
 		textAccueil.setEditable(false);
 		textAccueil.setContentType("text/html"); 
-		textAccueil.setText("<center><br /><br /><h1>BIENVENUE SUR L'APPLICATION THESAURUS 'CUISINE'</h1><center>  <center> <br/><br/><br /><br />R�alis�e par :  </center> <center>LAFON Lucas - MALMASSARI Pierre - DEPRET Axel</center> <center>CHAMBON Th�o - BRISSET Fabien - SAKO Sidiki</center><br /><br/><br/><br/><br/><br/><br/><br/><br/><br/><center> M1 INFORMATIQUE 2014-2015</center> <center>UNIVERSITE MONTPELLIER II</center> ");
+		textAccueil.setText("<center><br /><br /><h1>BIENVENUE SUR L'APPLICATION THESAURUS 'CUISINE'</h1><center>  <center> <br/><br/><br /><br />Réalisée par :  </center> <center>LAFON Lucas - MALMASSARI Pierre - DEPRET Axel</center> <center>CHAMBON Théo - BRISSET Fabien</center><br /><br/><br/><br/><br/><br/><br/><br/><br/><br/><center> M1 INFORMATIQUE 2014-2015</center> <center>UNIVERSITE MONTPELLIER II</center> ");
 	}
 	
-	public void afficher() {
+	public void afficher() 
+	{
 		super.afficher();
 		panAccueil.add(textAccueil);
+		panAccueil.setLayout(new BoxLayout(panAccueil, BoxLayout.X_AXIS));
 		panAccueil.revalidate();
 	}
 }
