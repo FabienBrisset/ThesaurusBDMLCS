78f2cd72375009f136bc15a65adb2840dd1f8e9b
Louk <louk@solydxk>
2014-11-27 23:10:57 +0100

    Possibilité de cliquer sur le père pour l'afficher

--- thesaurus/src/thesaurus/VueMot.java
+++ thesaurus/src/thesaurus/VueMot.java
@@ -257,28 +257,16 @@ public class VueMot {
 			}
 
 			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
+			public void mouseEntered(MouseEvent e) {}
 
 			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
+			public void mouseExited(MouseEvent e) {}
 
 			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
+			public void mousePressed(MouseEvent e) {}
 
 			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
+			public void mouseReleased(MouseEvent e) {}
 			
 		});
 		Controller.fenetre.getVueCourante().panConsulter.add(panParent);
